98d8909e1348528c363da6f2e89ea49d801cd39f
troy <4662Programming@gmail.com>
2016-02-04 16:10:00 -0800

    Loader changes

--- KnightByte/src/org/usfirst/frc4662/KnightByte/subsystems/Loader.java
+++ KnightByte/src/org/usfirst/frc4662/KnightByte/subsystems/Loader.java
@@ -48,5 +48,8 @@ public class Loader extends Subsystem {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
     }
+    // Spin loader wheels
+    
+    
 }
 
