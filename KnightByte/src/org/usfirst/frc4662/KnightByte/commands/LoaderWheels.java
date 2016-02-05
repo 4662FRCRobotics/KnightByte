46424dd6c81fae6a51ae272c10ee6a9bb50af12e
Gary Liao <gliao2@jhu.edu>
2016-02-04 16:58:52 -0800

    Command for loader wheels

--- KnightByte/src/org/usfirst/frc4662/KnightByte/commands/LoaderWheels.java
+++ KnightByte/src/org/usfirst/frc4662/KnightByte/commands/LoaderWheels.java
@@ -42,11 +42,13 @@ public class LoaderWheels extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	Robot.loader.spinLoaderWheels();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
